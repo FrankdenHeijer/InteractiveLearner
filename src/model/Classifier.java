8529335085b010f01d431c618c8fc7e84141df1c
Frank <denheijer.frank@gmail.com>
2016-12-29 11:00:44 +0100

    classifier

--- src/model/Classifier.java
+++ src/model/Classifier.java
@@ -1,7 +1,40 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Frank and Merijn on 20-12-16.
  */
-public class Classifier {
+public class Classifier implements controller.Protocol {
+
+    private HashMap<String, HashMap<String, Integer>> bagOfWords;
+    private double distinctWords;
+    private HashMap<String,Double> docsPerClass;
+    private HashMap<String,Double> totalWordsPerClass;
+    private double aantalDocsTotaal;
+
+
+    public Classifier() {
+        bagOfWords = new HashMap<String, HashMap<String, Integer>>();
+        totalWordsPerClass = new HashMap<String, Double>();
+        docsPerClass = new HashMap<String, Double>();
+    }
+
+    /**
+     * Counts the distinct amount of words of the classes combined
+     */
+    public void distinctWords() {
+        ArrayList<String> features = new ArrayList<String>();
+        for (classes c : classes.values()) {
+            String classname = c.name();
+            HashMap<String, Integer> subSet = bagOfWords.get(classname);
+            for (String s : subSet.keySet()) {
+                if (!features.contains(s)) {
+                    features.add(s);
+                }
+            }
+        }
+        distinctWords = new Integer(features.size()).doubleValue();
+    }
 }
